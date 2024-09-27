cb817d6b3e5e4fdb892a0417c128d6476a56054e
z4k23 <122319845+z4k23@users.noreply.github.com>
2024-09-27 19:04:32 +0200

    xd

---
+++ T1/PiedraPapelTijeras.js
@@ -0,0 +1,18 @@
+function playerElection(playerChoice) {
+    const computerChoice = Math.floor(Math.random() * 3);
+    console.log("Elección del jugador:", playerChoice);  
+    console.log("Elección de la computadora:", computerChoice);  
+    
+    if (playerChoice === computerChoice) {
+        console.log("¡Es un empate!");
+    } else if ((playerChoice === 0 && computerChoice === 2) || 
+               (playerChoice === 1 && computerChoice === 0) || 
+               (playerChoice === 2 && computerChoice === 1)) {
+        console.log("¡Has ganado!");
+    } else {
+        console.log("¡Ha ganado la computadora!");
+    }
+}
+
+
+playerElection(2);  
