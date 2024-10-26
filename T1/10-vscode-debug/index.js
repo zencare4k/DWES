a5d5ad60a130883d820984f02d97205359b80d2f
Carlos Marban <152007178+zencare4k@users.noreply.github.com>
2024-10-26 19:49:43 +0200

    xd

--- T1/10-vscode-debug/index.js
+++ T1/10-vscode-debug/index.js
@@ -1,5 +1,23 @@
+const fetch = require('node-fetch');
 
-const saludar = require('./app');
+async function fetchData() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+  const data = await response.json();
+  console.log(data); // Coloca un punto de ruptura aquí para inspeccionar `data`
+}
 
-const mensaje = saludar('Mundo'); 
-console.log(mensaje); 
+fetchData();
+
+const chalk = require('chalk');
+const myObject = {
+  propiedad1: "Hola",
+  propiedad2: 42,
+  metodo1() {
+    console.log("Método ejecutado");
+  },
+  onEvento() {
+    console.log("Evento disparado");
+  }
+};
+
+console.log(myObject); // Punto de ruptura aquí
