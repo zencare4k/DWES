920f98fa56311f6ff459cbcf6c8a2158c2d48cec
Carlos Marban <152007178+zencare4k@users.noreply.github.com>
2025-01-27 08:19:10 +0100

    Change

--- T2/24-websockets/index.js
+++ T2/24-websockets/index.js
@@ -3,7 +3,12 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ["GET", "POST"],
+  },
+});
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -16,12 +21,12 @@ io.on('connection', (socket) => {
     console.log('user disconnected');
   });
 
-  socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
+  socket.on('chat message', (data) => {
+    console.log('message: ' + data.message + ' from ' + data.nickname);
+    io.emit('chat message', data);
   });
 });
 
-server.listen(3000, () => {
+server.listen(3000, '0.0.0.0', () => {
   console.log('listening on *:3000');
 });
